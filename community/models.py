8e8d2cf3c88b44381064bb8a9b097482aaa3deec
Olloyor Makhammatnabiyev <muhammadnabiyevolloyor@gmail.com>
2025-01-03 17:19:07 +0500

    UI

--- community/models.py
+++ community/models.py
@@ -8,6 +8,9 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     image = models.ImageField(upload_to="posts/images/", blank=True, null=True)
+    images = models.ManyToManyField(
+        "PostImage", blank=True, related_name="posts_with_image"
+    )
     file = models.FileField(upload_to="posts/files/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -27,3 +30,8 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username}"
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_images")
+    image = models.ImageField(upload_to="post_images/")
